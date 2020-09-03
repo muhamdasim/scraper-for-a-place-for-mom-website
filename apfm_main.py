e79b27c6895e8c907118c295d48ea7f51e92d64f
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-09-03 12:13:04 +0500

    Request Library used

--- apfm_main.py
+++ apfm_main.py
@@ -1,13 +1,31 @@
 import pandas as pd
-
+import apfm_scraper as scraper
 
 #load urls from the file
 df=pd.read_csv('apfm-urls.csv')
 
-def all_urls:
+#function to push all the urls to list
+def allUrls():
     all_urls=[]
     for i in df['Address']:
         all_urls.append(i)
 
+    return all_urls
 
 
+
+
+#function_calling
+allUrls()
+#Requesting Urls
+soup=scraper.pageRequests()
+#Fetching Data
+
+#Fetching Page Title
+print(scraper.getPageTitle(soup))
+#Fetching Community Name
+print(scraper.getCommunityName(soup))
+#Fetching Meta Description
+print(scraper.getMetaDescription(soup))
+#Fetching Community Street Address
+print(scraper.getCommunityStreetAddress(soup))
--- apfm_scraper.py
+++ apfm_scraper.py
@@ -1,9 +1,27 @@
 import requests
 import bs4
-import apfm_main as ap
 
-r=requests.get('https://www.aplaceformom.com/community/merrill-gardens-at-first-hill-71358')
-soup=bs4.BeautifulSoup(r.text,'lxml')
+def listToString(s):
+    # initialize an empty string
+    str1 = " "
 
-metas = soup.find_all('meta')
-print ([ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ])
+    # return string
+    return (str1.join(s))
+
+def pageRequests():
+    r = requests.get('https://www.aplaceformom.com/community/merrill-gardens-at-first-hill-71358')
+    soup = bs4.BeautifulSoup(r.text, 'lxml')
+    return soup
+
+def getPageTitle(soup):
+    return soup.title.string.strip()
+
+def getCommunityName(soup):
+    return soup.find('div',class_='community-desc').h1.text
+
+def getMetaDescription(soup):
+    meta = soup.find_all('meta')
+    return listToString([meta.attrs['content'] for meta in meta if 'name' in meta.attrs and meta.attrs['name'] == 'description'])
+
+def getCommunityStreetAddress(soup):
+    return soup.find('div', class_='community-desc').p.text
